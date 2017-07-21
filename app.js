1fe34d52a1a8fe40078e5b8934b0245734c747f8
Skip Sauls <ssauls@ssauls-ltm2.internal.salesforce.com>
2017-07-21 01:13:04 -0700

    Testing main features

--- app.js
+++ app.js
@@ -4,14 +4,102 @@ var https = require('https');
 var http = require('http');
 var request = require('request');
 var app = express();
+var low = require('lowdb');
+var cookieSession = require('cookie-session');
+var bodyParser = require('body-parser');
+var uuidv4 = require('uuid/v4');
 
 var port = process.env.PORT || 3000;
 var https_port = process.env.HTTPS_PORT || parseInt(port) + 1;
 
+console.warn('process.env: ', process.env);
+
+// Localhost appId
+var appId = '3MVG9SemV5D80oBcff3jWxxK32b.valGtNTj90WK4mj5IAn1LOmdrz1ObgypNEnd9JRtxfhKpuE.iX7vv0WSy';
+
+
+// Check and set Heroku appId
+if (process.env.HEROKU === 'true') {
+	appId = '3MVG9SemV5D80oBcff3jWxxK32f4PQBwm702A4fEFlSAEviJg7BsC7PUI_WpupyyBwMfhXypJSkdVqKX7_IXr';
+}
+
+console.warn('appId: ', appId);
+
+var db = low('db.json');
+
+/*
+db.defaults({ posts: [], user: {} })
+	.write();
+
+db.get('posts')
+	.push({ id: 1, title: 'lowdb is awesome'})
+	.write();
+
+db.set('user.name', 'typicode')
+	.write();
+
+var foo = db.get('foo');
+console.warn('foo: ', foo, foo.push);
+if (!foo) {
+	db.set('foo', ["0"])
+		.write();
+}
+foo.push("1").push("2").write();
+foo.pop().write();
+*/
+
+app.set('view engine', 'ejs');
+
+app.set('views', './views');
+
 app.use(express.static(__dirname + '/public'));
 
+app.use(bodyParser.json())
+
+//app.use(cookieParser('waveiscool'));
+
+app.use(cookieSession({
+  name: 'session',
+  keys: ['analytics','wave','ltng','out'],
+
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}));
+
+app.use(express.static(__dirname + '/public'));
+
+
 app.get('/', function(req, res) {
-    res.sendfile('index.html');
+	var origin = req.query.origin;
+	console.warn('origin: ', origin);
+	if (!origin || origin.indexOf('lightning') < 0) {
+		res.render('pages/noorigin');
+	} else {
+		req.session.uuid = req.session.uuid || uuidv4();
+	    res.render('pages/index', {title: 'Analytics Lightning Out Playground', appId: appId, origin: origin});		
+	}
+});
+
+app.get('/test', function(req, res) {
+	req.session.uuid = req.session.uuid || uuidv4();
+	req.session.count = (req.session.count || 0)+ 1;
+	console.warn('req.session: ', req.session);
+    res.render('pages/test', {title: 'Test', count: req.session.count, appId: req.session.appId});
+});
+
+app.post('/appid', function(req, res) {
+    console.warn("req.params: ", req.params);
+    console.warn("req.body: ", req.body);
+
+    var body = req.body; //req.params.json;
+    var json = JSON.stringify(req.body);
+
+    console.warn("json: ", json);
+
+    req.session.appId = body.appId;
+
+    res.send('success');
+
 });
 
 // Create an HTTP service
---
+++ public/javascripts/util.js
@@ -0,0 +1,76 @@
+function getRequestBaseURL() {
+    return '';
+}
+
+function request(obj, successHandler, errorHandler) {
+
+    console.warn('request: ', obj);
+
+    var method = obj.method || 'GET',
+        xhr = new XMLHttpRequest(),
+        url = getRequestBaseURL();
+
+    // dev friendly API: Add leading '/' if missing so url + path concat always works
+    if (obj.path && obj.path.charAt(0) !== '/' && obj.path.indexOf('http') < 0) {
+        obj.path = '/' + obj.path;
+    }
+    url = url + obj.path;
+
+    console.warn('url: ', url);
+
+    if (obj.params) {
+        url += '?' + toQueryString(obj.params);
+    }
+
+    var self = this;
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if (xhr.status > 199 && xhr.status < 300) {
+                if (successHandler) {
+                    var resp = undefined;
+                    if (xhr.responseText) {
+                        resp = xhr.responseText || undefined;
+                        try {
+                            resp = JSON.parse(xhr.responseText);
+                        } catch (e) {}
+                    }                    
+                    successHandler(resp);
+                }
+            } else {
+                var error = xhr.responseText ? JSON.parse(xhr.responseText) : {message: 'An error has occurred'};
+                if (errorHandler) {
+                    errorHandler(error);
+                }
+            }
+        }
+    };
+
+    xhr.open(method, url, true);
+    if (obj.accept) {
+        xhr.setRequestHeader("Accept", obj.accept);
+    } else {
+        xhr.setRequestHeader("Accept", "application/json");
+    }
+    
+    if (obj.contentType) {
+        xhr.setRequestHeader("Content-Type", obj.contentType);
+    } else {
+        xhr.setRequestHeader("Content-Type", "application/json");
+    }
+
+    if (obj.headers) {
+        for (var k in obj.headers) {
+            console.warn("setting header: ", k, obj.headers[k]); 
+            xhr.setRequestHeader(k, obj.headers[k]);            
+        }
+    }
+    console.warn('sending: ', obj.data);
+    xhr.send(obj.data ? JSON.stringify(obj.data) : undefined);
+}
+
+/*
+$.urlParam = function(name){
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    return results[1] || 0;
+}
+*/
